0203b84d65d391b922e455cf07e900cab1c79793
Иван Красиков <ivan1.krasikov.2003@gmail.com>
2022-12-12 00:36:46 +0300

    Добавил статическое поле tmpCard, для передачи значений карт на поле

--- app/src/main/java/com/developlstu/durakonline/views/CardsView.kt
+++ app/src/main/java/com/developlstu/durakonline/views/CardsView.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import com.developlstu.durakonline.R
 import com.developlstu.durakonline.constants.Suit
@@ -20,6 +21,10 @@ class CardsView : View {
     private var value: Cards? = null
     private var widthCard: Int = 100
     private var heightCard: Int = 200
+
+    companion object{
+        var tmpCard: Cards? = null
+    }
 
     constructor(ctx: Context, atts: AttributeSet):
         super(ctx, atts)
@@ -47,32 +52,43 @@ class CardsView : View {
             color = Color.WHITE
         }
         canvas?.drawPaint(paint)
-        if(value != null) {
+        if (value != null) {
             paint.apply {
-                color = if (Suit.CLUBS == value?.suit || Suit.SPADES == value?.suit) Color.BLACK else Color.RED
+                color =
+                    if (Suit.CLUBS == value?.suit || Suit.SPADES == value?.suit) Color.BLACK else Color.RED
                 style = Paint.Style.FILL
                 isAntiAlias = true
-                textSize =  Math.min(widthCard,heightCard)*10/(25F)
+                textSize = Math.min(widthCard, heightCard) * 10 / (25F)
             }
-            canvas?.drawText("${value!!.value}${value!!.suit.suit}", (widthCard/130F)*10, (heightCard/50F)*10, paint)
-            canvas?.drawText("${value!!.value}${value!!.suit.suit}", (widthCard/130F)*10, heightCard.toFloat() - 20F, paint)
+            canvas?.drawText(
+                "${value!!.value}${value!!.suit.suit}",
+                (widthCard / 130F) * 10,
+                (heightCard / 50F) * 10,
+                paint
+            )
+            canvas?.drawText(
+                "${value!!.value}${value!!.suit.suit}",
+                (widthCard / 130F) * 10,
+                heightCard.toFloat() - 20F,
+                paint
+            )
 
-        } else if(this.id != R.id.deck){
+        } else if (this.id != R.id.deck) {
             paint.apply {
                 color = Color.BLACK
                 style = Paint.Style.FILL
                 isAntiAlias = true
-                textSize =  Math.min(widthCard,heightCard)*10/(25F)
+                textSize = Math.min(widthCard, heightCard) * 10 / (25F)
             }
-            canvas?.drawText("?", widthCard/2F - 10F, heightCard/2F, paint)
-        }else if(this.id == R.id.deck){
+            canvas?.drawText("?", widthCard / 2F - 10F, heightCard / 2F, paint)
+        } else if (this.id == R.id.deck) {
             paint.apply {
                 color = Color.BLACK
                 style = Paint.Style.FILL
                 isAntiAlias = true
-                textSize =  Math.min(widthCard,heightCard)*10/(25F)
+                textSize = Math.min(widthCard, heightCard) * 10 / (25F)
             }
-            canvas?.drawText("${Deck.Companion.size}", widthCard/2F - 20F, heightCard/2F, paint)
+            canvas?.drawText("${Deck.Companion.size}", widthCard / 2F - 20F, heightCard / 2F, paint)
         }
     }
 

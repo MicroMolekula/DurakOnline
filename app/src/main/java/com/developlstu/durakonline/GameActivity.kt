2f22b6a7617697425883c8ecc5218429b8ab84aa
Иван Красиков <ivan1.krasikov.2003@gmail.com>
2022-12-12 00:37:30 +0300

    Перенес основную логику в GameModel, также создаю объекты карт на поле

--- app/src/main/java/com/developlstu/durakonline/GameActivity.kt
+++ app/src/main/java/com/developlstu/durakonline/GameActivity.kt
@@ -5,23 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.*
+import android.widget.Button
+import android.widget.LinearLayout
 import com.developlstu.durakonline.views.CardsView
 import com.developlstu.durakonline.constants.Suit
 import com.developlstu.durakonline.constants.cardValue
 import com.developlstu.durakonline.models.Cards
 import com.developlstu.durakonline.models.Deck
+import com.developlstu.durakonline.models.GameModel
 import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         supportActionBar?.hide()
-        val deck: Deck = Deck()
-        deck.outputDeck()
-
-
+        val deck = Deck()
 
         val cardsEnemy: Array<CardsView?> = Array(6, {null})
         cardsEnemy[0] = findViewById(R.id.enemyCard1)
@@ -38,43 +38,37 @@ class GameActivity : AppCompatActivity() {
         cardsPlayer[3] = findViewById(R.id.card4)
         cardsPlayer[4] = findViewById(R.id.card5)
         cardsPlayer[5] = findViewById(R.id.card6)
-        initCardsPlayer(cardsPlayer, deck)
+
+        val cardsOnField: Array<CardsView?> = Array(6, {null})
+        cardsOnField[0] = findViewById(R.id.cardonfield1)
+        cardsOnField[1] = findViewById(R.id.cardonfield2)
+        cardsOnField[2] = findViewById(R.id.cardonfield3)
+        cardsOnField[3] = findViewById(R.id.cardonfield4)
+        cardsOnField[4] = findViewById(R.id.cardonfield5)
+        cardsOnField[5] = findViewById(R.id.cardonfield6)
+
+        val cardsOnFieldEnemy: Array<CardsView?> = Array(6, {null})
+        cardsOnFieldEnemy[0] = findViewById(R.id.cardonfieldenemy1)
+        cardsOnFieldEnemy[1] = findViewById(R.id.cardonfieldenemy2)
+        cardsOnFieldEnemy[2] = findViewById(R.id.cardonfieldenemy3)
+        cardsOnFieldEnemy[3] = findViewById(R.id.cardonfieldenemy4)
+        cardsOnFieldEnemy[4] = findViewById(R.id.cardonfieldenemy5)
+        cardsOnFieldEnemy[5] = findViewById(R.id.cardonfieldenemy6)
 
         val deckView: CardsView = findViewById(R.id.deck)
         val trump: CardsView = findViewById(R.id.trump)
-        trump.setValue(deck.takeTrump())
+        val btnBito: Button = findViewById(R.id.btnbito)
 
-
-        cardsPlayer[0]?.setOnTouchListener(this::hideCard)
-        cardsPlayer[1]?.setOnTouchListener(this::hideCard)
-        cardsPlayer[2]?.setOnTouchListener(this::hideCard)
-        cardsPlayer[3]?.setOnTouchListener(this::hideCard)
-        cardsPlayer[4]?.setOnTouchListener(this::hideCard)
-        cardsPlayer[5]?.setOnTouchListener(this::hideCard)
+        val gameModel = GameModel(
+            cardsPlayer,
+            cardsEnemy,
+            cardsOnField,
+            cardsOnFieldEnemy,
+            deck,
+            trump
+        )
+        gameModel.play()
 
     }
 
-    private fun initCardsPlayer(array: Array<CardsView?>, deck: Deck){
-        for(i in 0 until 6){
-            array[i]?.setValue(deck.takeCard())
-        }
-    }
-
-    private fun hideCard(view: View, event: MotionEvent): Boolean{
-        view.visibility = View.INVISIBLE
-        return true
-    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
